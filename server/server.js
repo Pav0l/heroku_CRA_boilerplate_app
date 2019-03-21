07b9b00f04c80aca8cc42b71fb4496e5f7af5d52
Pav0l <pavol.karas@gmail.com>
2019-03-21 13:53:39 +0100

    Initial project clean up and setup

---
+++ server/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config(); //require and configure dotenv.
+const express = require('express');
+const path = require( 'path');
+
+const app = express();
+
+
+const PORT = process.env.PORT || 5555;
+const pathToIndexHtml = path.join(__dirname,'build', 'index.html');
+const pathToBuildFolder = path.join(__dirname, 'build');
+
+app.use(express.static(pathToBuildFolder));
+
+
+app.get('/', (req, res) => {
+  // res.sendFile(pathToIndexHtml);
+  res.send("Hi there");
+})
+
+app.listen(PORT, () => console.log(`Express app listening at http://localhost:${PORT}`));
